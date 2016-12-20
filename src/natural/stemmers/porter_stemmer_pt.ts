7dbed03f5989bd8fdc98730dc7db22f81809057f
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-12-20 11:04:11 -0800

    Add this-types to porter stemmer PT

--- src/natural/stemmers/porter_stemmer_pt.ts
+++ src/natural/stemmers/porter_stemmer_pt.ts
@@ -21,91 +21,87 @@ THE SOFTWARE.
 */
 
 import Token = require('./token');
-module.exports = (function () {
-  'use strict';
+import Stemmer = require('./stemmer_pt');
+'use strict';
 
-  var Stemmer     = require('./stemmer_pt'),
-    PorterStemmer = new Stemmer();
-
-  /**
-   * Marks a region after the first non-vowel following a vowel, or the
-   * null region at the end of the word if there is no such non-vowel.
-   *
-   * @param {Object} token Token to stem.
-   * @param {Number} start Start index (defaults to 0).
-   * @param {Number}       Region start index.
-   */
-    var markRegionN = function (start: number) {
+/**
+ * Marks a region after the first non-vowel following a vowel, or the
+ * null region at the end of the word if there is no such non-vowel.
+ *
+ * @param token Token to stem.
+ * @param start Start index (defaults to 0).
+ * @return      Region start index.
+ */
+function markRegionN(this: Token, start: number) {
     var index = start || 0,
-      length = this.string.length,
-      region = length;
+    length = this.string.length,
+    region = length;
 
     while (index < length - 1 && region === length) {
-      if (this.hasVowelAtIndex(index) && !this.hasVowelAtIndex(index + 1)) {
-        region = index + 2;
-      }
-      index++;
+        if (this.hasVowelAtIndex(index) && !this.hasVowelAtIndex(index + 1)) {
+            region = index + 2;
+        }
+        index++;
     }
 
     return region;
-  };
+};
 
-  /**
-   * Mark RV.
-   *
-   * @param  {Object} token Token to stem.
-   * @return {Number}       Region start index.
-   */
-  var markRegionV = function () {
+/**
+ * Mark RV.
+ *
+ * @param  {Object} token Token to stem.
+ * @return {Number}       Region start index.
+ */
+function markRegionV(this: Token) {
     var rv = this.string.length;
 
     if (rv > 3) {
-      if (!this.hasVowelAtIndex(1)) {
-        rv = this.nextVowelIndex(2) + 1;
+        if (!this.hasVowelAtIndex(1)) {
+            rv = this.nextVowelIndex(2) + 1;
 
-      } else if (this.hasVowelAtIndex(0) && this.hasVowelAtIndex(1)) {
-        rv = this.nextConsonantIndex(2) + 1;
+        } else if (this.hasVowelAtIndex(0) && this.hasVowelAtIndex(1)) {
+            rv = this.nextConsonantIndex(2) + 1;
 
-      } else {
-        rv = 3;
-      }
+        } else {
+            rv = 3;
+        }
     }
 
     return rv;
-  };
+};
 
-  /**
-   * Prelude.
-   *
-   * Nasalised vowel forms should be treated as a vowel followed by a consonant.
-   */
-    function prelude (token: Token) {
+/**
+ * Prelude.
+ *
+ * Nasalised vowel forms should be treated as a vowel followed by a consonant.
+ */
+function prelude (token: Token) {
     return token
-    .replaceAll('ã', 'a~')
-    .replaceAll('õ', 'o~');
-  }
+        .replaceAll('ã', 'a~')
+        .replaceAll('õ', 'o~');
+}
 
-  /**
-   * Step 1: Standard suffix removal.
-   *
-   * This step should always be performed.
-   *
-   * @param  token Word to stem.
-   * @return       Stemmed token.
-   */
-    function standardSuffix (token: Token) {
-
+/**
+ * Step 1: Standard suffix removal.
+ *
+ * This step should always be performed.
+ *
+ * @param  token Word to stem.
+ * @return       Stemmed token.
+ */
+function standardSuffix (token: Token) {
     token.replaceSuffixInRegion([
-      'amentos', 'imentos', 'aço~es', 'adoras', 'adores', 'amento', 'imento',
+        'amentos', 'imentos', 'aço~es', 'adoras', 'adores', 'amento', 'imento',
 
-      'aça~o', 'adora', 'ância', 'antes', 'ismos', 'istas',
+        'aça~o', 'adora', 'ância', 'antes', 'ismos', 'istas',
 
-      'ador', 'ante', 'ável', 'ezas', 'icas', 'icos', 'ismo', 'ista', 'ível',
-      'osas', 'osos',
+        'ador', 'ante', 'ável', 'ezas', 'icas', 'icos', 'ismo', 'ista', 'ível',
+        'osas', 'osos',
 
-      'eza', 'ica', 'ico', 'osa', 'oso'
+        'eza', 'ica', 'ico', 'osa', 'oso'
 
-      ], '', 'r2');
+    ], '', 'r2');
 
     token.replaceSuffixInRegion(['logias', 'logia'], 'log', 'r2');
 
@@ -114,163 +110,165 @@ module.exports = (function () {
     token.replaceSuffixInRegion(['ências', 'ência'], 'ente', 'r2');
 
     token.replaceSuffixInRegion([
-      'ativamente', 'icamente', 'ivamente', 'osamente', 'adamente'
+        'ativamente', 'icamente', 'ivamente', 'osamente', 'adamente'
     ], '', 'r2');
 
     token.replaceSuffixInRegion('amente', '', 'r1');
 
     token.replaceSuffixInRegion([
-      'antemente', 'avelmente', 'ivelmente', 'mente'
+        'antemente', 'avelmente', 'ivelmente', 'mente'
     ], '', 'r2');
 
     token.replaceSuffixInRegion([
-      'abilidades', 'abilidade',
-      'icidades', 'icidade',
-      'ividades', 'ividade',
-      'idades', 'idade'
+        'abilidades', 'abilidade',
+        'icidades', 'icidade',
+        'ividades', 'ividade',
+        'idades', 'idade'
     ], '', 'r2');
 
     token.replaceSuffixInRegion([
-      'ativas', 'ativos', 'ativa', 'ativo',
-      'ivas', 'ivos', 'iva', 'ivo'
+        'ativas', 'ativos', 'ativa', 'ativo',
+        'ivas', 'ivos', 'iva', 'ivo'
     ], '', 'r2');
 
     if (token.hasSuffix('eiras') || token.hasSuffix('eira')) {
-      token.replaceSuffixInRegion(['iras', 'ira'], 'ir', 'rv');
+        token.replaceSuffixInRegion(['iras', 'ira'], 'ir', 'rv');
     }
 
     return token;
-  }
+}
 
-  /**
-   * Step 2: Verb suffix removal.
-   *
-   * Perform this step if no ending was removed in step 1.
-   *
-   * @param  token   Token to stem.
-   * @return         Stemmed token.
-   */
-    function verbSuffix (token: Token) {
+/**
+ * Step 2: Verb suffix removal.
+ *
+ * Perform this step if no ending was removed in step 1.
+ *
+ * @param  token   Token to stem.
+ * @return         Stemmed token.
+ */
+function verbSuffix (token: Token) {
 
     token.replaceSuffixInRegion([
-      'aríamos', 'ássemos', 'eríamos', 'êssemos', 'iríamos', 'íssemos',
+        'aríamos', 'ássemos', 'eríamos', 'êssemos', 'iríamos', 'íssemos',
 
-      'áramos', 'aremos', 'aríeis', 'ásseis', 'ávamos', 'éramos', 'eremos',
-      'eríeis', 'ésseis', 'íramos', 'iremos', 'iríeis', 'ísseis',
+        'áramos', 'aremos', 'aríeis', 'ásseis', 'ávamos', 'éramos', 'eremos',
+        'eríeis', 'ésseis', 'íramos', 'iremos', 'iríeis', 'ísseis',
 
-      'ara~o', 'ardes', 'areis', 'áreis', 'ariam', 'arias', 'armos', 'assem',
-      'asses', 'astes', 'áveis', 'era~o', 'erdes', 'ereis', 'éreis', 'eriam',
-      'erias', 'ermos', 'essem', 'esses', 'estes', 'íamos', 'ira~o', 'irdes',
-      'ireis', 'íreis', 'iriam', 'irias', 'irmos', 'issem', 'isses', 'istes',
+        'ara~o', 'ardes', 'areis', 'áreis', 'ariam', 'arias', 'armos', 'assem',
+        'asses', 'astes', 'áveis', 'era~o', 'erdes', 'ereis', 'éreis', 'eriam',
+        'erias', 'ermos', 'essem', 'esses', 'estes', 'íamos', 'ira~o', 'irdes',
+        'ireis', 'íreis', 'iriam', 'irias', 'irmos', 'issem', 'isses', 'istes',
 
-      'adas', 'ados', 'amos', 'ámos', 'ando', 'aram', 'aras', 'arás', 'arei',
-      'arem', 'ares', 'aria', 'asse', 'aste', 'avam', 'avas', 'emos', 'endo',
-      'eram', 'eras', 'erás', 'erei', 'erem', 'eres', 'eria', 'esse', 'este',
-      'idas', 'idos', 'íeis', 'imos', 'indo', 'iram', 'iras', 'irás', 'irei',
-      'irem', 'ires', 'iria', 'isse', 'iste',
+        'adas', 'ados', 'amos', 'ámos', 'ando', 'aram', 'aras', 'arás', 'arei',
+        'arem', 'ares', 'aria', 'asse', 'aste', 'avam', 'avas', 'emos', 'endo',
+        'eram', 'eras', 'erás', 'erei', 'erem', 'eres', 'eria', 'esse', 'este',
+        'idas', 'idos', 'íeis', 'imos', 'indo', 'iram', 'iras', 'irás', 'irei',
+        'irem', 'ires', 'iria', 'isse', 'iste',
 
-      'ada', 'ado', 'ais', 'ara', 'ará', 'ava', 'eis', 'era', 'erá', 'iam',
-      'ias', 'ida', 'ido', 'ira', 'irá',
+        'ada', 'ado', 'ais', 'ara', 'ará', 'ava', 'eis', 'era', 'erá', 'iam',
+        'ias', 'ida', 'ido', 'ira', 'irá',
 
-      'am', 'ar', 'as', 'ei', 'em', 'er', 'es', 'eu', 'ia', 'ir', 'is', 'iu', 'ou'
+        'am', 'ar', 'as', 'ei', 'em', 'er', 'es', 'eu', 'ia', 'ir', 'is', 'iu', 'ou'
 
     ], '', 'rv');
 
     return token;
-  }
+}
 
-  /**
-   * Step 3: Delete suffix i.
-   *
-   * Perform this step if the word was changed, in RV and preceded by c.
-   *
-   * @param  token   Token to stem.
-   * @return         Stemmed token.
-   */
-    function iPrecededByCSuffix (token: Token) {
+/**
+ * Step 3: Delete suffix i.
+ *
+ * Perform this step if the word was changed, in RV and preceded by c.
+ *
+ * @param  token   Token to stem.
+ * @return         Stemmed token.
+ */
+function iPrecededByCSuffix (token: Token) {
 
     if (token.hasSuffix('ci')) {
-      token.replaceSuffixInRegion('i', '', 'rv');
+        token.replaceSuffixInRegion('i', '', 'rv');
     }
 
     return token;
-  }
+}
 
-  /**
-   * Step 4: Residual suffix.
-   *
-   * Perform this step if steps 1 and 2 did not alter the word.
-   *
-   * @param  token Token to stem.
-   * @return       Stemmed token.
-   */
-    function residualSuffix(token: Token) {
+/**
+ * Step 4: Residual suffix.
+ *
+ * Perform this step if steps 1 and 2 did not alter the word.
+ *
+ * @param  token Token to stem.
+ * @return       Stemmed token.
+ */
+function residualSuffix(token: Token) {
 
     token.replaceSuffixInRegion(['os', 'a', 'i', 'o', 'á', 'í', 'ó'], '', 'rv');
 
     return token;
-  }
+}
 
-  /**
-   * Step 5: Residual form.
-   *
-   * This step should always be performed.
-   *
-   * @param  token Token to stem.
-   * @return       Stemmed token.
-   */
-    function residualForm(token: Token) {
+/**
+ * Step 5: Residual form.
+ *
+ * This step should always be performed.
+ *
+ * @param  token Token to stem.
+ * @return       Stemmed token.
+ */
+function residualForm(token: Token) {
 
     var tokenString = token.string;
 
     if (token.hasSuffix('gue') || token.hasSuffix('gué') || token.hasSuffix('guê')) {
-      token.replaceSuffixInRegion(['ue', 'ué', 'uê'], '', 'rv');
+        token.replaceSuffixInRegion(['ue', 'ué', 'uê'], '', 'rv');
     }
 
     if (token.hasSuffix('cie') || token.hasSuffix('cié') || token.hasSuffix('ciê')) {
-      token.replaceSuffixInRegion(['ie', 'ié', 'iê'], '', 'rv');
+        token.replaceSuffixInRegion(['ie', 'ié', 'iê'], '', 'rv');
     }
 
     if (tokenString === token.string) {
-      token.replaceSuffixInRegion(['e', 'é', 'ê'], '', 'rv');
+        token.replaceSuffixInRegion(['e', 'é', 'ê'], '', 'rv');
     }
 
     token.replaceSuffixInRegion('ç', 'c', 'all');
 
     return token;
-  }
+}
 
-  /**
-   * Postlude.
-   *
-   * Turns a~, o~ back into ã, õ.
-   *
-   * @param  token Word to stem.
-   * @return       Stemmed token.
-   */
-    function postlude (token: Token) {
+/**
+ * Postlude.
+ *
+ * Turns a~, o~ back into ã, õ.
+ *
+ * @param  token Word to stem.
+ * @return       Stemmed token.
+ */
+function postlude (token: Token) {
     return token
-      .replaceAll('a~', 'ã')
-      .replaceAll('o~', 'õ');
-  }
+        .replaceAll('a~', 'ã')
+        .replaceAll('o~', 'õ');
+}
 
-  /**
-   * Stems a word using a Porter stemmer algorithm.
-   *
-   * @param  word Word to stem.
-   * @return      Stemmed token.
-   */
-    PorterStemmer.stem = function (word: string) {
+const PorterStemmer = new Stemmer();
+
+/**
+ * Stems a word using a Porter stemmer algorithm.
+ *
+ * @param  word Word to stem.
+ * @return      Stemmed token.
+ */
+PorterStemmer.stem = function(word: string) {
     var token = new Token(word.toLowerCase()),
-      original;
+    original;
 
     token = prelude(token);
 
     token.usingVowels('aeiouáéíóúâêôàãõ')
-      .markRegion('all', 0)
-      .markRegion('r1', null, markRegionN)
-      .markRegion('r2', token.regions['r1'], markRegionN)
-      .markRegion('rv', null, markRegionV);
+        .markRegion('all', 0)
+        .markRegion('r1', null, markRegionN)
+        .markRegion('r2', token.regions['r1'], markRegionN)
+        .markRegion('rv', null, markRegionV);
 
     original = token.string;
 
@@ -279,7 +277,7 @@ module.exports = (function () {
 
     // Do step 2 if no ending was removed by step 1.
     if (token.string === original) {
-      token = verbSuffix(token);
+        token = verbSuffix(token);
     }
 
     // If the last step to be obeyed — either step 1 or 2 — altered the word,
@@ -293,7 +291,6 @@ module.exports = (function () {
     token = postlude(token);
 
     return token.string;
-  }
+}
 
-  return PorterStemmer;
-})();
+export = PorterStemmer;
